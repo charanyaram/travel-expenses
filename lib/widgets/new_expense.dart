ee7dd359517fca5da4fff77a19c79d4a54096278
charanyaram <charanya.ramakrishnan@mq.edu.au>
2024-03-24 10:49:03 +1100

    Video 6

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,10 +10,13 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
 
-  void _saveEnteredTitle(String inputText) {
-    _enteredTitle = inputText;
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose(){
+    _titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -23,7 +26,7 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveEnteredTitle,
+            controller: _titleController,
             maxLength: 45,
             decoration: const InputDecoration(
               label: Text('Title'),
@@ -33,7 +36,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                 onPressed: () {
-                  print(_enteredTitle);
+                  print(_titleController.text);
                 },
                 child: const Text('Save Expense'),
               ),
